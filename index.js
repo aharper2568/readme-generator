979717a402956a9913b1f111d84356049a4d6a5c
aharper2568 <anthonyharper568@gmail.com>
2024-07-04 20:59:20 -0400

    added function to generate readme and filled in initializing function

--- index.js
+++ index.js
@@ -24,6 +24,19 @@ const questions = [
     message: 'Enter your email address:',
   },
 ];
+function generateREADME(answers) {
+
+  return `
+  # ${answers.title}
+  ## Usage
+${answers.usage}
+## Questions
+If you have any questions, you can reach me at:
+- GitHub: [${answers.github}](https://github.com/${answers.github})
+- Email: ${answers.email}
+  `
+
+}
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
@@ -36,7 +49,16 @@ function writeToFile(fileName, data) {
 }
 
 // TODO: Create a function to initialize app
-function init() {}
-
+function init() {
+  inquirer
+    .prompt(questions)
+    .then((answers) => {
+      const readmeContent = generateREADME(answers);
+      writeToFile('README.md', readmeContent);
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+}
 // Function call to initialize app
 init();
