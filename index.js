06cea890ef7996b1f0ebf47dbdfa2436c92bea5a
aharper2568 <anthonyharper568@gmail.com>
2024-07-04 20:47:58 -0400

    added packages and wrote writeToFile function

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require('fs');
 // TODO: Create an array of questions for user input
 const questions = [];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) => {
+    if (err) {
+      return console.log(err);
+    }
+    console.log('README.md has been generated');
+  });
+}
 
 // TODO: Create a function to initialize app
 function init() {}
