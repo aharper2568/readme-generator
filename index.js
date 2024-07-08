1fab6e0175c885b1052841d677bf8333e3472ed3
aharper2568 <anthonyharper568@gmail.com>
2024-07-08 13:17:21 -0400

    added more questions to questions array

--- index.js
+++ index.js
@@ -10,8 +10,34 @@ const questions = [
   },
   {
     type: 'input',
+    name: 'description',
+    message: 'Enter a description of your project:',
+  },
+  {
+    type: 'input',
+    name: 'installation',
+    message: 'Enter the installation instructions:',
+  },
+  {
+    type: 'input',
     name: 'usage',
     message: 'Enter the usage information:',
+  },
+  {
+    type: 'input',
+    name: 'contributing',
+    message: 'Enter the contribution guidelines:',
+  },
+  {
+    type: 'input',
+    name: 'tests',
+    message: 'Enter the test instructions:',
+  },
+  {
+    type: 'list',
+    name: 'license',
+    message: 'Choose a license for your application:',
+    choices: ['MIT', 'GPLv2', 'GPLv3', 'Apache', 'BSD 3-Clause', 'None'],
   },
   {
     type: 'input',
